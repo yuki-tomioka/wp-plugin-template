5ea2f37ff835320b3f6b175355d1713f647e7cde
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2019-07-29 11:46:42 +0900

    基本構成の作成

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: (プラグインの名前)
+Plugin URI: (プラグインの説明と更新を示すページの URI)
+Description: (プラグインの短い説明)
+Version: (プラグインのバージョン番号。例: 1.0)
+Author: yuki tomioka
+Author URI: (プラグイン作者の URI)
+License: GPL2
+*/
+
+/*  Copyright 2019 yuki tomioka (email : yuki.tomioka.1016@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+     published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
